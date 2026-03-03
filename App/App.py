c7a4332300c9542f404b75a5c587045feca2abd1
hetuk2005 <patelhetuk2005@gmail.com>
2026-03-03 20:38:57 +0530

    Code

--- App/App.py
+++ App/App.py
@@ -129,4 +129,5 @@ def scan_email():
 # RUN APP
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
