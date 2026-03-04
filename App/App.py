e24bbd833e138169cbb94f02c662ff7172619bf7
hetuk2005 <patelhetuk2005@gmail.com>
2026-03-04 12:23:10 +0530

    Code

--- App/App.py
+++ App/App.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import joblib
 import os
+from flask_cors import CORS;
 
 app = Flask(__name__)
+CORS(app)
 
 # =========================
 # INTENT PATTERNS
